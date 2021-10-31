c0b71c7e0e9dea56968c089ded8fec0f5e4bc70a
Adeline Mersch <mersch@umn.edu>
2021-10-31 16:49:08 -0500

    add practice

---
+++ src/practicewk2/practicewk2.java
@@ -0,0 +1,25 @@
+package practicewk2;
+
+public class practicewk2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int tokens = 12;
+//int costToPlayArcadeGame = 3;
+
+//System.out.println(tokens < costToPlayArcadeGame);
+	//}
+		
+int num = 15;
+String words = "hello world";
+String word = "programming";
+
+if (num < word.length() || words.length() > num) {
+	System.out.println(true);
+} else {
+	System.out.println(false);
+}
+}
+
+
+}
